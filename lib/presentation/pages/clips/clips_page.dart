757ec139d85895f09924ecba33bb1c54e114778e
Sergio López Ceballos <sergioloc2@gmail.com>
2026-09-03 18:31:07 +0200

    Show clip pending time with units

--- lib/presentation/pages/clips/clips_page.dart
+++ lib/presentation/pages/clips/clips_page.dart
@@ -235,10 +235,12 @@ class _ClipsPageState extends State<ClipsPage> {
   }
 
   String _formatCountdown(Duration duration) {
-    final hours = duration.inHours.toString().padLeft(2, '0');
-    final minutes = (duration.inMinutes % 60).toString().padLeft(2, '0');
-    final seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
-    return '$hours:$minutes:$seconds';
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes % 60;
+    final parts = <String>[];
+    if (hours > 0) parts.add('${hours}h');
+    parts.add('${minutes}min');
+    return parts.join(' ');
   }
 }
 
