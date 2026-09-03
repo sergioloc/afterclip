5c2c3b9234a31618c30e537d305638d7968e79f7
Sergio López Ceballos <sergioloc2@gmail.com>
2026-09-03 17:34:05 +0200

    Show clips count in home screen

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,21 +1,59 @@
 import 'package:flutter/material.dart';
+import '../../../data/datasource/local/clip_local_datasource.dart';
+import '../../../data/repositories/clip_repository_impl.dart';
+import '../../../domain/usecases/get_all_clips_usecase.dart';
 import '../camera/camera_page.dart';
 import '../clips/clips_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _clipCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadClipCount();
+  }
+
+  Future<void> _loadClipCount() async {
+    final useCase = GetAllClipsUseCase(
+      ClipRepositoryImpl(ClipLocalDatasource()),
+    );
+    final clips = await useCase.execute();
+    if (mounted) {
+      setState(() => _clipCount = clips.length);
+    }
+  }
+
+  Future<void> _openClips() async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ClipsPage()),
+    );
+    _loadClipCount();
+  }
+
+  Future<void> _openCamera() async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const CameraPage()),
+    );
+    await Future.delayed(const Duration(milliseconds: 200));
+    await _loadClipCount();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const CameraPage()),
-            );
-          },
+          onTap: _openCamera,
           child: const Scaffold(
             backgroundColor: Colors.black,
           ),
@@ -24,12 +62,7 @@ class HomePage extends StatelessWidget {
           bottom: 32,
           right: 24,
           child: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ClipsPage()),
-              );
-            },
+            onTap: _openClips,
             child: Container(
               width: 56,
               height: 56,
@@ -37,10 +70,15 @@ class HomePage extends StatelessWidget {
                 shape: BoxShape.circle,
                 color: Colors.transparent,
               ),
-              child: const Icon(
-                Icons.movie_outlined,
-                color: Colors.white,
-                size: 28,
+              child: Center(
+                child: Text(
+                  _clipCount.toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ),
